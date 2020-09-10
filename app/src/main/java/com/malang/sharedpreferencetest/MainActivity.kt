020eaa3171286e79848cbdb2a64507ed387f11b5
Han Kyoung Min <48062593+Malang2@users.noreply.github.com>
2020-09-10 10:35:33 +0900

    feat : first project
    start project

---
+++ app/src/main/java/com/malang/sharedpreferencetest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.malang.sharedpreferencetest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
