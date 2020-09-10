a914951fd52bb59e3eb324d36838f3d1318142c6
Han Kyoung Min <48062593+Malang2@users.noreply.github.com>
2020-09-10 11:56:33 +0900

    feat : save button
    if click save button, show data saved preerences

--- app/src/main/java/com/malang/sharedpreferencetest/MainActivity.kt
+++ app/src/main/java/com/malang/sharedpreferencetest/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.malang.sharedpreferencetest
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Preferences.data = testEdit.text.toString()
         Log.e("data", Preferences.data)
+
+        saveButton.setOnClickListener {
+            if (testEdit.text.toString().isBlank())
+                Toast.makeText(this, "비어있음.", Toast.LENGTH_LONG).show()
+            else {
+                Preferences.data = testEdit.text.toString()
+                Toast.makeText(this, Preferences.data, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
